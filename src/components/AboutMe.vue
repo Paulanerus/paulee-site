2c74891d924997e12c471b78ee7f9a2ddae2aa38
Paul <reallucycode@gmail.com>
2022-03-01 10:05:32 +0100

    Changed Font in AboutMe

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -3,7 +3,7 @@
         <div class="p-4 m-4">
             <h1 class="font-roboto text-default-dark text-6xl -mb-2">Hello there!</h1>
             <p
-                class="text-lg"
+                class="text-lg font-maven"
             >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
         </div>
     </div>
