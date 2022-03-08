8ef4e81799f6001ff5b421a5c9eccc831c85c64c
Paul <reallucycode@gmail.com>
2022-03-08 20:10:23 +0100

    Added an AboutMe text

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -2,9 +2,12 @@
     <div class="mt-14 shadow-lg rounded-md max-w-2xl mx-auto container-anim">
         <div class="p-4 m-4">
             <h1 class="font-roboto text-default-dark text-6xl -mb-2">Hello there!</h1>
-            <p
-                class="text-lg font-maven"
-            >Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+            <p class="text-lg font-maven">
+                My name is Paul and I am a {{ age }} year old computer science student from Germany.
+                In school I was already interested in software development and quickly found my way to
+                Java. Since then I deepened my knowledge in other languages/frameworks, like Kotlin, Python or Vue.
+                I plan to finish my bachelor in spring 2025.
+            </p>
         </div>
     </div>
 </template>
@@ -12,6 +15,15 @@
 <script>
 export default {
     name: 'AboutMe',
+    data() {
+        return {
+            age: 0,
+        };
+    },
+    mounted() {
+        const date = new Date();
+        this.age = date.getFullYear() - 2002 - (date.getDay() >= 16 && date.getMonth() >= 7 ? 0 : 1);
+    },
 }
 </script>
 
